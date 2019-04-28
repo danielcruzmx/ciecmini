0f7b7dd85562b2f0b557350bafc5fbc71bb27644
danielcruzmx <danielcruzmx@hotmail.com>
2019-04-27 20:47:24 -0500

    datos de catalogos

--- ciecbase/Catalogos/admin.py
+++ ciecbase/Catalogos/admin.py
@@ -1,8 +1,6 @@
 import csv
 from django.http import HttpResponse
 from django.contrib import admin
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
--- ciecbase/ciecbase/settings.py
+++ ciecbase/ciecbase/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
